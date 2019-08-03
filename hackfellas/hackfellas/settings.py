d702943022ed92bd4120d68f23cd68cc72fd6190
Stanislav Hrytcyshyn <grucushun90@gmail.com>
2019-08-03 23:49:55 +0300

    Set settings file.Created route to index page with rendering of index template

--- hackfellas/hackfellas/settings.py
+++ hackfellas/hackfellas/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # custom apps
+    'intro.apps.IntroConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'hackfellas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'hackfellas/static')
+]
--- hackfellas/intro/views.py
+++ hackfellas/intro/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request=request,
+                  template_name='intro/index.html',
+                  context={})
